f8017eec45d98163ae42372f3404d8b9ccc2483d
DriftCloud <3291382264@qq.com>
2026-06-20 14:13:20 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useThemeStore } from './stores/theme'
+
+const themeStore = useThemeStore()
+
+onMounted(() => {
+  themeStore.initTheme()
+})
+</script>
+
+<template>
+  <RouterView />
+</template>

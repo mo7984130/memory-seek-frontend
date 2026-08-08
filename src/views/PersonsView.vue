15c1ab01262006e2698c4908d2cc0e88c788159b
DriftCloud <3291382264@qq.com>
2026-08-08 22:54:49 +0800

    style(persons): shrink cards via extra column instead of ratio
    人物封面恢复 1:1，改为在同等视口下比照片流多一列，使卡片
    整体更小而非改变宽高比。

--- src/views/PersonsView.vue
+++ src/views/PersonsView.vue
@@ -77,12 +77,14 @@ function handleResize() {
 
   if (containerWidth.value < 640) {
     columnCount.value = 2;
-  } else if (containerWidth.value < 1024) {
+  } else if (containerWidth.value < 768) {
     columnCount.value = 3;
-  } else if (containerWidth.value < 1440) {
+  } else if (containerWidth.value < 1024) {
     columnCount.value = 4;
-  } else {
+  } else if (containerWidth.value < 1440) {
     columnCount.value = 5;
+  } else {
+    columnCount.value = 6;
   }
 }
 
@@ -136,10 +138,10 @@ function loadMoreWaterfall(): Promise<number> {
 }
 
 /**
- * 人物卡片高度：4:3 封面 + 底部信息区
+ * 人物卡片高度：1:1 封面 + 底部信息区
  */
 function personCardHeight(_item: WaterfallItem, colWidth: number): number {
-  return colWidth * 0.75 + 64;
+  return colWidth + 64;
 }
 
 /**
@@ -428,7 +430,7 @@ onActivated(async () => {
 }
 
 .person-card__cover {
-  aspect-ratio: 4 / 3;
+  aspect-ratio: 1 / 1;
   display: flex;
   align-items: center;
   justify-content: center;
